6bf6f88ca6e16c08300dc5ba92b04df052074202
DefiledSpec <defiledspec@gmail.com>
2025-01-20 04:28:15 -0600

    update sql

--- setup/weed_plants.sql
+++ setup/weed_plants.sql
@@ -1,18 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.17-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             11.2.0.6213
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
--- Dumping structure for table void-main.v_weed_plants
 CREATE TABLE IF NOT EXISTS `v_weed_plants` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `stage` tinyint(4) NOT NULL DEFAULT 1,
@@ -23,13 +8,6 @@ CREATE TABLE IF NOT EXISTS `v_weed_plants` (
   `coords` tinytext NOT NULL,
   `harvestable` bit(1) NOT NULL DEFAULT b'0',
   `dead` bit(1) NOT NULL DEFAULT b'0',
-  `last_updated` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4;
-
--- Data exporting was unselected.
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8mb4;
